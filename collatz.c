a1a2ba050c2a96ca389dd3b5fd8ddd749a9860ea
Miteuk <47401437+Miteuk@users.noreply.github.com>
2024-09-24 12:07:18 -0700

    some code for main

---
+++ collatz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+
+int collatz_steps(int n, int test)
+{
+    int steps = 0;
+
+    while (n != 1){
+        
+
+
+
+
+        
+    }
+}
+
+int main(int argc, char *argv[]){
+
+    int num = atoi(argv[1]);
+    int MIN = atoi(argv[2]);
+    int MAX = atoi(argb[3]);
+
+    srand(time(NULL));
+
+    printf("Random Number, Steps\n");
+
+    for (int ix = 0; i < num, i++){
+      
+        int value = rand() % (MAX - MIN + 1) + MIN;  //Random value between min and max
+        int step
+    }
+
+}
